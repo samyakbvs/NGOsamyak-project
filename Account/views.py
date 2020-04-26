a61753a996bf00a95f39e0246bcea8d227db3369
Samyak <samyakjainbvs@gmail.com>
2020-04-26 14:37:44 +0530

    git push

--- Account/views.py
+++ Account/views.py
@@ -85,9 +85,8 @@ def Donate(request):
             'INDUSTRY_TYPE_ID':'Retail',
             'WEBSITE':'WEBSTAGING',
             'CHANNEL_ID':'WEB',
-	        # 'CALLBACK_URL':'http://165.22.216.110/Account/ThankYou/DONATION/'+str(donation.uid)+'/',
+	        'CALLBACK_URL':'http://165.22.216.110/Account/ThankYou/DONATION/'+str(donation.uid)+'/',
             # 'CALLBACK_URL':'http://127.0.0.1:8000/Account/ThankYou/DONATION/'+str(donation.uid)+'/',
-	        'CALLBACK_URL':'http://localhost/Account/ThankYou/DONATION/'+str(donation.uid)+'/',
 
         }
 
@@ -122,9 +121,8 @@ def SiteForm(request,detailSite_id):
             'INDUSTRY_TYPE_ID':'Retail',
             'WEBSITE':'WEBSTAGING',
             'CHANNEL_ID':'WEB',
-	        # 'CALLBACK_URL':'http://165.22.216.110/Account/ThankYou/KIND/'+str(kind_donation.uid)+'/',
+	        'CALLBACK_URL':'http://165.22.216.110/Account/ThankYou/KIND/'+str(kind_donation.uid)+'/',
             # 'CALLBACK_URL':'http://127.0.0.1:8000/Account/ThankYou/KIND/'+str(kind_donation.uid)+'/',
-	        'CALLBACK_URL':'http://localhost/Account/ThankYou/KIND/'+str(kind_donation.uid)+'/',
 
         }
 
